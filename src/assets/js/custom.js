dcd906cbd7a08f66df9f0c015bfb5e3231b5630f
unknown <vietnamthaotranvan@gmail.com>
2020-03-25 00:30:58 +0700

    e

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,8 +1,4 @@
-/**
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 var CURRENT_URL = window.location.href.split('?')[0],
     $BODY = $('body'),
