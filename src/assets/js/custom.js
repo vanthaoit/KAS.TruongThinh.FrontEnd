cb5bf4367506df8e4d60251e94f2bbab0f4b1872
thao tran <vietnamthaotranvan@gmail.com>
2020-05-19 22:01:09 +0700

    fix

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -14,6 +14,30 @@ $(document).ready(function () {
 
     // var audio = document.getElementById("player");
     // audio.volume = 0.01;
+    initialLoading = function(){
+        $("#flexiselDemo").flexisel({
+            visibleItems: 3,
+            animationSpeed: 1000,
+            autoPlay: true,
+            autoPlaySpeed: 3000,
+            pauseOnHover: true,
+            enableResponsiveBreakpoints: true,
+            responsiveBreakpoints: {
+                portrait: {
+                    changePoint: 480,
+                    visibleItems: 1
+                },
+                landscape: {
+                    changePoint: 640,
+                    visibleItems: 2
+                },
+                tablet: {
+                    changePoint: 768,
+                    visibleItems: 2
+                }
+            }
+        });
+    }
     $('.popup-with-zoom-anim').magnificPopup({
         type: 'inline',
         fixedContentPos: false,
@@ -25,6 +49,8 @@ $(document).ready(function () {
         removalDelay: 300,
         mainClass: 'my-mfp-zoom-in'
     });
+    
+
 
     // TODO: This is some kind of easy fix, maybe we can improve this
     var setContentHeight = function () {
